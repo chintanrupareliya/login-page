05290f405f2a3a122e57db8b22e3848c701755a4
chintanrupareliya <ckrupareliya2002@gmail.com>
2023-06-19 21:27:20 +0530

    Add continue with Google functionality and some bug fix

--- FRONTEND/login.js
+++ FRONTEND/login.js
@@ -86,8 +86,45 @@ async function singupuser(event) {
       }),
     }).then((res) => res.json());
     console.log(result);
-    sessionStorage.setItem("jwtToken", result);
+    if (result) {
+      window.location.href = "./login.html";
+    }
   } else {
     alert("confirm password is not same as password");
   }
+}
+
+// function for client implimentaion
+function oauthSignIn() {
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement("form");
+  form.setAttribute("method", "GET"); // Send as a GET request.
+  form.setAttribute("action", oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {
+    client_id: "YOUR_GOOGLE-API_CLIENT_ID",
+    redirect_uri: "http://127.0.0.1:5501/FRONTEND/main.html",
+    response_type: "token",
+    scope:
+      "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+    include_granted_scopes: "true",
+    state: "pass-through value",
+  };
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement("input");
+    input.setAttribute("type", "hidden");
+    input.setAttribute("name", p);
+    input.setAttribute("value", params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
 }
--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -7,9 +7,47 @@ fetch("http://localhost:3030/api/auth/getuser", {
 })
   .then((res) => res.json())
   .then((userdata) => {
-    let user = document.querySelector(".info");
+    let user = document.querySelector(".user");
     user.innerHTML = `Welcome ${userdata.name} ${userdata.email}`;
   });
 window.addEventListener("beforeunload", () => {
   sessionStorage.removeItem("jwtToken");
 });
+// login with google code
+const urlParams = new URLSearchParams(window.location.hash.substring(1));
+const accessToken = urlParams.get("access_token");
+urlParams.forEach((value, key) => {
+  localStorage.setItem(key, value);
+  window.location.href = "http://127.0.0.1:5501/FRONTEND/main.html";
+});
+
+//function for logout
+
+let logout = document.querySelector(".logout");
+logout.addEventListener("click", () => {
+  sessionStorage.removeItem("jwtToken");
+  localStorage.clear();
+  location.reload();
+});
+
+//function for show user ditails on page
+
+const img = document.createElement("img");
+const info = document.querySelector(".info");
+if (localStorage.getItem("access_token") != null) {
+  fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      let user = document.querySelector(".user");
+      user.innerHTML = data.given_name;
+      img.setAttribute("src", data.picture);
+      info.prepend(img);
+    })
+    .catch((error) => {
+      console.log("Error fetching user data:", error);
+    });
+}
